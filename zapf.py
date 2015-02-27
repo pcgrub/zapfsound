092cbc22f5716d26547a9ac24c1e51763a5077f1
stehla <joe-der-elch@gmx.net>
2015-02-27 11:05:15 +0100

    killswitch testing

--- zapf.py
+++ zapf.py
@@ -7,17 +7,15 @@ from sys import exit
 from numpy import loadtxt
 
 """Init(GPIO and audio):"""
-GPIO.setmode(GPIO.BCM)						
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)	
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)	
-GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)	
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)	
-GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)	
-GPIO.setup(17, GPIO.OUT)				
+pins=[25,24,23,18,15,14,4,17,27,22] #pins for for soundboard buttons
+GPIO.setmode(GPIO.BCM)
+for d in range(len(pins)):
+    GPIO.setup(pins[d], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setuo(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #killswitch pin 8
+GPIO.setup(17, GPIO.OUT) #pin for green LED
 GPIO.output(17,0)					
 pygame.mixer.init(48000, -16, 1, 1024)
 Channel0 = pygame.mixer.Channel(0)
-pins=[25,24,23,18,15]
 
 
 """reading in the sound file names from config and creating the sounds:"""
@@ -35,13 +33,15 @@ for i in range(0, len(adresses)-1):
 
 """switch detection and playing sounds:"""
 print "fertig"
-GPIO.output(17,1)
+GPIO.output(17,1) #switching on green LED when ready
 while True:
 	try:
-		for i in range(len(pins)):
-        		if GPIO.input(pins[i]):
-				print i
-				Channel0.play(soundlist[i])
-				sleep(soundlist[i].get_length())
+        for i in range(len(pins)):
+            if GPIO.input(pins[i]): #testing soundpins
+            print i
+            Channel0.play(soundlist[i])
+            sleep(soundlist[i].get_length())
+        if GPIO.inpit(8):  #testing killswitch pin 8
+            raise KeyboardInterrupt
 	except KeyboardInterrupt:
 		GPIO.cleanup()
