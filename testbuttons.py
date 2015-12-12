f1c81c25f20cfa31a7a3138c1379bd439bc926e1
stehla <joe-der-elch@gmx.net>
2015-12-12 23:05:43 +0100

    various changes

--- testbuttons.py
+++ testbuttons.py
@@ -9,20 +9,17 @@ def setting_pins_up(pins):
     GPIO.setmode(GPIO.BCM)
     for pin in pins:
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # killswitch pin 8
-    GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # special pin 7
-    GPIO.setup(0, GPIO.OUT)  # pin for green LED
-    GPIO.setup(1, GPIO.OUT)  # pin for yellow LED
-    GPIO.output(0, 0)
-    GPIO.output(1, 1)
 
-pins = [18, 17, 21, 22, 23, 24, 10, 9, 11, 25, 8, 7] #pins for for soundboard buttons
+pins=[11,9,4,14,15,18,17,27,22,23,24,10]
+#pins = [9, 11, 4, 14, 15, 18, 17, 27, 22, 23, 24, 10] #pins for for soundboard buttons
 setting_pins_up(pins)
 while True:
     try:
         for i in range(len(pins)):
                 if GPIO.input(pins[i]): #testing soundpins
-                    print i
+                    print i+1
                     sleep(0.3)
     except KeyboardInterrupt:
         GPIO.cleanup()
+	break
+
