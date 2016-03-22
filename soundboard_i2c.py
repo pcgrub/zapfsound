77e63fe598418689fae4b6e15c58325d4bdbc340
stehla <joe-der-elch@gmx.net>
2016-03-22 19:43:21 +0100

    first version, yet to be tested

--- soundboard_i2c.py
+++ soundboard_i2c.py
@@ -1,8 +1,9 @@
 import smbus
-import numpy as np
 from time import sleep
+import subprocess
+import pygame.mixer
 
-buttons=[15,6,16,7,5,1,4,2,3,8]
+buttons=[14,5,15,6,4,0,3,1,2,7]
 bus = smbus.SMBus(1)
 
 DEVICE = 0x20 # Device address (A0-A2)
@@ -17,14 +18,65 @@ def setup_buttons():
 	bus.write_byte_data(DEVICE,IODIRB,0xF8) # Set last 5 pins of GPIOB to inputs
 
 def check_switch(number):
-	if number < 8:
+	try:
+		number //= 1
+	except TypeError:
+		print 'Only numbers, please!'
+	if buttons[number] < 8:
 		state_a = bus.read_byte_data(DEVICE,GPIOA)
-		a = 2**buttons[number] & state_a
-		if a == 2**buttons[number]:
+		actual = buttons[number]
+		a = 2**actual & state_a
+		if a == 2**actual:
 			return 1
-	elif number > 7:
+	elif buttons[number] > 7:
 		state_b = bus.read_byte_data(DEVICE,GPIOB)
-		b = 2**buttons[number] & state_b
-		if b == 2**buttons[number]:
+		actual = buttons[number]-8
+		b = 2**actual & state_b
+		if b == 2**actual:
 			return 1
 	return 0
+
+def reading_in(pins):
+    # reading in the sound file names from config and creating the sounds:
+    soundfile = open('sounds.txt', 'r')
+    adresses = soundfile.read().splitlines()
+    soundlist = []
+    for i in range(len(adresses)-1):
+        if adresses[i][0] != '#':
+            print str(adresses[i])
+            temp = pygame.mixer.Sound(str(adresses[i]))
+            if (temp.get_length() >= 5 or temp.get_length() <= 0.1):
+                temp = pygame.mixer.Sound("WilhelmScream.wav")
+            soundlist.append(temp)
+    if len(soundlist) < 10:
+        for i in range(len(soundlist),len(pins)):
+            soundlist.append(pygame.mixer.Sound("WilhelmScream.wav"))
+    return soundlist
+
+def main():
+    # switch detection and playing sounds:
+    pygame.mixer.init(48000, -16, 1, 1024)
+    Channel0 = pygame.mixer.Channel(0)
+    buttons=[14,5,15,6,4,0,3,1,2,7]
+    soundlist = reading_in(buttons)
+    print "fertig"
+    # GPIO.output(0,1) #switching on green LED when ready
+    while True:
+        try:
+            for i in range(len(buttons)):
+                if check_switch(i): #testing soundpins
+                    Channel0.play(soundlist[i])
+                    sleep(soundlist[i].get_length())
+                if check_switch(13):
+                    try:
+                        subprocess.call('git pull', shell=True)
+                        reading_in(buttons)
+                    except:
+                        pass
+                if check_switch(12):  #testing killswitch button
+                    raise KeyboardInterrupt
+        except KeyboardInterrupt:
+            break
+
+if __name__=="__main__":
+    main()
