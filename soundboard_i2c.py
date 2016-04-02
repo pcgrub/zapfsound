53d3fc2539ffadcdda87f27f24f8713af9817cd7
stehla <joe-der-elch@gmx.net>
2016-04-02 20:37:36 +0200

    some changes I do not remember

--- soundboard_i2c.py
+++ soundboard_i2c.py
@@ -11,12 +11,12 @@ IODIRA = 0x00 # Pin direction register A
 IODIRB = 0x01 # Pin direction register B
 GPIOA  = 0x12 # Register for inputs GPIOA
 GPIOB  = 0x13 # Register for inputs GPIOB
-
+OLATB  = 0x15 # Register for outputs on B
 
 def setup_buttons():
 	bus.write_byte_data(DEVICE,IODIRA,0xFF) # Set all pins on GPIOA to inputs
 	bus.write_byte_data(DEVICE,IODIRB,0xF8) # Set last 5 pins of GPIOB to inputs
-
+ 	bus.write_byte_data(DEVICE,OLATB,0)
 def check_switch(number):
 	try:
 		number //= 1
@@ -41,16 +41,18 @@ def reading_in(pins):
     soundfile = open('sounds.txt', 'r')
     adresses = soundfile.read().splitlines()
     soundlist = []
+    path = '/home/pi/wavsounds/'
     for i in range(len(adresses)-1):
         if adresses[i][0] != '#':
             print str(adresses[i])
-            temp = pygame.mixer.Sound(str(adresses[i]))
+            temp = pygame.mixer.Sound(path+str(adresses[i]))
             if (temp.get_length() >= 5 or temp.get_length() <= 0.1):
-                temp = pygame.mixer.Sound("WilhelmScream.wav")
+                temp = pygame.mixer.Sound(path+"WilhelmScream.wav")
             soundlist.append(temp)
     if len(soundlist) < 10:
         for i in range(len(soundlist),len(pins)):
-            soundlist.append(pygame.mixer.Sound("WilhelmScream.wav"))
+            soundlist.append(pygame.mixer.Sound(path+"WilhelmScream.wav"))
+    soundfile.close()
     return soundlist
 
 def main():
@@ -60,23 +62,25 @@ def main():
     buttons=[14,5,15,6,4,0,3,1,2,7]
     soundlist = reading_in(buttons)
     print "fertig"
-    # GPIO.output(0,1) #switching on green LED when ready
+    bus.write_byte_data(DEVICE,OLATB,1) # switching on green LED
     while True:
         try:
             for i in range(len(buttons)):
                 if check_switch(i): #testing soundpins
                     Channel0.play(soundlist[i])
                     sleep(soundlist[i].get_length())
-                if check_switch(13):
-                    try:
-                        subprocess.call('git pull', shell=True)
-                        reading_in(buttons)
-                    except:
-                        pass
-                if check_switch(12):  #testing killswitch button
-                    raise KeyboardInterrupt
+               # if check_switch(13):
+               #     try:
+               #         subprocess.call('git pull', shell=True)
+               #         reading_in(buttons)
+               #     except:
+               #         pass
+               # if check_switch(12):  #testing killswitch button
+               #     raise KeyboardInterrupt
         except KeyboardInterrupt:
-            break
+            bus.write_byte_data(DEVICE,OLATB,4)
+	    break
 
 if __name__=="__main__":
     main()
+
--- zapf.py
+++ zapf.py
@@ -27,9 +27,9 @@ def reading_in(pins):
     for i in range(len(adresses)-1):
         if adresses[i][0] != '#':
             print str(adresses[i])
-            temp = pygame.mixer.Sound(str(adresses[i]))
+            temp = pygame.mixer.Sound("/home/pi/wavsounds/"+str(adresses[i]))
             if (temp.get_length() >= 5 or temp.get_length() <= 0.1):
-                temp = pygame.mixer.Sound("WilhelmScream.wav")
+                temp = pygame.mixer.Sound("/home/pi/wavsounds/WilhelmScream.wav")
             soundlist.append(temp)
     if len(soundlist) < 10:
         for i in range(len(soundlist),len(pins)):
